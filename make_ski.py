735e8d53ebd7dda4b5c492f0488eb0ed1254386e
PabloCorcho <corchocaballeropablo@gmail.com>
2021-07-06 17:55:23 +0200

    .

---
+++ make_ski.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  6 17:48:40 2021
+
+@author: pablo
+"""
+
+import sys
+sys.path.insert(0, '../')
+import pigs
+
+pigs.SkirtFile('ski_params', output_path='my_skirt_file')
